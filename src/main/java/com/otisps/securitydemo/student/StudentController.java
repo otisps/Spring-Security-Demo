b03d405770c00550cf94a2d4243628dd864533de
jokerotis <63875651+jokerotis@users.noreply.github.com>
2022-04-13 20:45:34 +0100

    #1.0 Demo app for security - this push blank application for accessing students.

---
+++ src/main/java/com/otisps/securitydemo/student/StudentController.java
@@ -0,0 +1,27 @@
+package com.otisps.securitydemo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "James Crewe"),
+            new Student(2, "John Doe"),
+            new Student(3, "Jack Erwin"),
+            new Student(4, "Joshua Golf")
+    );
+    @GetMapping(path="{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId){
+        return STUDENTS.stream().filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student " + studentId + " does not exist!"));
+    }
+}
