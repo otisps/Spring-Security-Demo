bbf37d75c5b4d6aee3365a9d7a45be9d68aec3a5
jokerotis <63875651+jokerotis@users.noreply.github.com>
2022-04-13 22:49:50 +0100

    #7.3 Roles and Permissions: Defining as Enums

--- src/main/java/com/otisps/securitydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/otisps/securitydemo/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.otisps.securitydemo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +9,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.otisps.securitydemo.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired private final PasswordEncoder passwordEncoder;
+
+    @Autowired private final PasswordEncoder superSlowPasswordEncoder;
+
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, PasswordEncoder superSlowPasswordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+        this.superSlowPasswordEncoder = superSlowPasswordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,11 +40,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+
     @Override
     @Bean
     protected UserDetailsService userDetailsService() {
-        UserDetails annaSmithUser=
-                User.builder().username("otisps").password("password") .roles("STUDENT").build();
-        return new InMemoryUserDetailsManager(annaSmithUser);
+        UserDetails otisUser=
+                User.builder()
+                        .username("otisps").password(passwordEncoder.encode("password"))
+                        .roles(STUDENT.name()).build();
+
+        UserDetails otisAdmin = User.builder()
+                .username("otis").password(passwordEncoder.encode("password"))
+                .roles(ADMIN.name()).build();
+        return new InMemoryUserDetailsManager(otisUser, otisAdmin);
     }
 }
