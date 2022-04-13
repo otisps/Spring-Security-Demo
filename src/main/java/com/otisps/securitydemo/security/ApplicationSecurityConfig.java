df8eba5d4ca1995a438f1f1a7faa1858f2acdeb2
jokerotis <63875651+jokerotis@users.noreply.github.com>
2022-04-13 21:46:51 +0100

    #7.1 User with username otisps using InMemoryUserDetailsManager for our UserDetailsService config method.
    Doesn't work right now as password not encoded.

--- src/main/java/com/otisps/securitydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/otisps/securitydemo/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.otisps.securitydemo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -21,5 +26,11 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
-
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser=
+                User.builder().username("otisps").password("password") .roles("STUDENT").build();
+        return new InMemoryUserDetailsManager(annaSmithUser);
+    }
 }
