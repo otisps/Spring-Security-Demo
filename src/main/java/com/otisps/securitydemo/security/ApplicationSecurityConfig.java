7879fa14409d87eb80c4b43de5b9edfaa4bb37c0
jokerotis <63875651+jokerotis@users.noreply.github.com>
2022-04-14 20:52:01 +0100

    #9.6 Logout through our own link

--- src/main/java/com/otisps/securitydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/otisps/securitydemo/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -50,7 +51,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .rememberMe()
                 .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-                .key("somethingVerySecure");
+                .key("somethingVerySecure")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me")
+                .logoutSuccessUrl("/login");
     }
 
 
