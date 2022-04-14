664e48cabe0b04382c142c8e56aa50a45183ee3e
jokerotis <63875651+jokerotis@users.noreply.github.com>
2022-04-14 15:32:36 +0100

    #9.5  rememberMe with custom validity time & custom key for securing the hash

--- src/main/java/com/otisps/securitydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/otisps/securitydemo/security/ApplicationSecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.otisps.securitydemo.security.ApplicationUserRole.*;
 
 @Configuration
@@ -44,7 +46,11 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/courses", true);
+                .defaultSuccessUrl("/courses", true)
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                .key("somethingVerySecure");
     }
 
 
