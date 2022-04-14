599c3606e80f31243975997bf02639d5b52fc9de
jokerotis <63875651+jokerotis@users.noreply.github.com>
2022-04-14 12:41:10 +0100

    #8 Example with Cross Site Request Forgery Protection. https://docs.spring.io/spring-security/site/docs/5.0.x/reference/html/csrf.html

--- src/main/java/com/otisps/securitydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/otisps/securitydemo/security/ApplicationSecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 
 import static com.otisps.securitydemo.security.ApplicationUserRole.*;
 
@@ -33,7 +35,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // TODO : Amigo will teach this
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*" , "/js/*").permitAll()
                 .antMatchers("/api/students").hasRole(STUDENT.name())
--- src/main/java/com/otisps/securitydemo/security/PasswordConfig.java
+++ src/main/java/com/otisps/securitydemo/security/PasswordConfig.java
@@ -10,7 +10,7 @@ public class PasswordConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder()   {
-        return new BCryptPasswordEncoder(10);
+        return new BCryptPasswordEncoder(5);
     }
 
 }
