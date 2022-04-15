22c8283b9aa4deac4ce9a5631dfb03575e4d9367
jokerotis <63875651+jokerotis@users.noreply.github.com>
2022-04-15 10:33:51 +0100

    #10.2 Base Setup

--- src/main/java/com/otisps/securitydemo/auth/ApplicationUser.java
+++ src/main/java/com/otisps/securitydemo/auth/ApplicationUser.java
@@ -31,7 +31,7 @@ public class ApplicationUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return grantedAuthorities;
     }
 
     @Override
